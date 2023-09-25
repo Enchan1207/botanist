4df850e029ebb827f0da819d9fb6fe87b9751ea7
Enchan <enchant0130@gmail.com>
2023-09-25 15:09:47 +0900

    [Update #5] configure test for AVR

---
+++ tests_avr/test_analyse.cpp
@@ -0,0 +1,20 @@
+//
+// 数式解析のテスト
+//
+#include <string.h>
+
+#include <collection2/list.hpp>
+
+#include "analyser.hpp"
+#include "testcase.hpp"
+#include "tokenizer.hpp"
+
+using namespace botanist;
+
+namespace botanisttests {
+
+const TestFunction tests[] = {};
+
+const size_t testCount = 0;
+
+}  // namespace botanisttests
